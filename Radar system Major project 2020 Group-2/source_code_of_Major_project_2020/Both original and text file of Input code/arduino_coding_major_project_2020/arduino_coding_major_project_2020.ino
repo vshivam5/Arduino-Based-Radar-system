4ff7eaed67acf2be225d18ca14bc0898add8bb61
Shivam Verma <55866265+vshivam5@users.noreply.github.com>
2021-02-10 18:02:47 +0530

    All details of the project with code and reports

---
+++ Radar system Major project 2020 Group-2/source_code_of_Major_project_2020/Both original and text file of Input code/arduino_coding_major_project_2020/arduino_coding_major_project_2020.ino
@@ -0,0 +1,50 @@
+// Includes the Servo library
+#include <Servo.h>. 
+// Defines Tirg and Echo pins of the Ultrasonic Sensor
+const int trigPin = 10;
+const int echoPin = 11;
+// Variables for the duration and the distance
+long duration;
+int distance;
+Servo myServo; // Creates a servo object for controlling the servo motor
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600);
+  myServo.attach(12); // Defines on which pin is the servo motor attached
+}
+void loop() {
+  // rotates the servo motor from 15 to 165 degrees
+  for(int i=20;i<=210;i++){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  
+  Serial.print(i); // Sends the current degree into the Serial Port
+  Serial.print(","); 
+  Serial.print(distance); 
+  Serial.print("."); 
+  }
+  // Repeats the previous lines from 165 to 15 degrees
+  for(int i=210;i>30;i--){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+  }
+}
+// Function for calculating the distance measured by the Ultrasonic sensor
+int calculateDistance(){ 
+  
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
+  distance= duration*0.034/2;
+  return distance;
+}
